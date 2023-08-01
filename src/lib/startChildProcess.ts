e51f9d5aec726363a30bb068c6b92c51ae7e2703
Luhut Andreas <andreas.luhut@brilife.co.id>
2023-08-01 17:47:13 +0700

    Add cp cwd

--- src/lib/startChildProcess.ts
+++ src/lib/startChildProcess.ts
@@ -3,8 +3,8 @@ import config from "../config";
 
 export default function startChildProcess() {
   [...config.command.build, ...config.command.start].forEach((cmd) => {
-    console.log(`Executing '${cmd}'`);
-    const stdout = execSync(cmd);
+    console.log(`Executing '${cmd}' at ${process.cwd()}`);
+    const stdout = execSync(cmd, { cwd: process.cwd() });
     console.log(stdout.toString());
   });
 }
